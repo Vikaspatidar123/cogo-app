584352162dce835fc8d2765f3d35f78c8f0a4393
Pavan-cogo <pavan.sai@cogoport.com>
2023-02-01 17:09:23 +0530

    login page structure fixed and signup page created

---
+++ ui/page-components/authentication/Signup/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import LeftPanel from '../../common/LeftPanel';
+
+import styles from './styles.module.css';
+
+function Signup() {
+	return (
+		<div className={styles.container}>
+			<div className={styles.left_container}>
+				<LeftPanel />
+			</div>
+			<div className={styles.right_container}>
+				hiiii
+			</div>
+		</div>
+	);
+}
+
+export default Signup;

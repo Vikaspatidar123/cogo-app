3421352adfdb3991d2893dfa7855403e7063e59d
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-02-21 20:46:30 +0530

    fix

--- ui/commons/components/AddressForm/utils/getValue.js
+++
@@ -1,9 +0,0 @@
-import { getByKey } from '@cogoport/utils';
-
-const getValue = (object, path, defaultValue) => {
-	const value = getByKey(object, path, defaultValue);
-
-	return value === null ? defaultValue : value;
-};
-
-export default getValue;
--- ui/page-components/profile/components/ProfileDetails/AccountDetails/DetailsTabs/TeamDetails/index.js
+++
@@ -1,4 +0,0 @@
-function TeamDetails() {
-	return <div>TeamDetails</div>;
-}
-export default TeamDetails;

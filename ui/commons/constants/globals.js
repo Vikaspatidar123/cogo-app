9a5113b2d7562fe611ef69457fb7822bf1116d64
Roh-Patil <Rohit.Patil@cogoport.com>
2023-02-23 15:25:24 +0530

    converting globals.json to globals.js

---
+++ ui/commons/constants/globals.js
@@ -0,0 +1,61 @@
+const GLOBAL_CONSTANTS = {
+	country_entity_ids: {
+		IN: '6fd98605-9d5d-479d-9fac-cf905d292b88',
+	},
+	country_ids: {
+		IN: '541d1232-58ce-4d64-83d6-556a42209eb7',
+	},
+	currency_code: {
+		INR : 'INR',
+		USD : 'USD',
+		EUR : 'EUR',
+		GBP : 'GBP',
+		AED : 'AED',
+	},
+
+	formats: {
+		date: {
+			'dd/MM/yyyy'  : 'dd/MM/yyyy',
+			'dd MMM yyyy' : 'dd MMM yyyy',
+			MMM           : 'MMM',
+		},
+		time: {
+			'hh:mm aaa' : 'hh:mm aaa',
+			'HH:mm'     : 'HH:mm',
+		},
+	},
+
+	container_type_size: {
+		ft20: {
+			standard : '20ft Standard',
+			refer    : '20ft Reefer',
+		},
+		ft40: {
+			standard : '40ft Standard',
+			refer    : '40ft Reefer',
+		},
+		hc40: {
+			standard: '40HC Standard',
+		},
+		hc45: {
+			standard: '45HC Standard',
+		},
+	},
+	units: {
+		weight: {
+			single   : 'kg',
+			multiple : 'kgs',
+			inMt     : 'MT',
+		},
+		volume: {
+			gross : 'cc',
+			total : 'cbm',
+		},
+		length: {
+			m  : 'm',
+			ft : 'FT',
+		},
+	},
+};
+
+export default GLOBAL_CONSTANTS;

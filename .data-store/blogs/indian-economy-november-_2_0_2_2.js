e624e42445c22de20af268c90a4901c64a4d1702
Roh-Patil <Rohit.Patil@cogoport.com>
2023-02-23 16:02:08 +0530

    removing gitignore files

--- .data-store/blogs/indian-economy-november-_2_0_2_2.js
+++
@@ -1,8 +0,0 @@
-export default {
-    "slug": "indian-economy-november-2022",
-    "name": "Indian Economy – November 2022",
-    "image": {
-        "url": "https://uploads-ssl.webflow.com/5e57ba59552cf400c593fd16/63c660d1b8051f33f576e986_Indian%20Economy1.jpg"
-    },
-    "short-overview": "Emerging market economies (EMEs) continue to experience unpredictable capital flows, and global spill overs endanger growth prospects."
-};

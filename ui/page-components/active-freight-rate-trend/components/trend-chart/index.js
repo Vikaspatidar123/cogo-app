fe97a1b5f0924a4ab7fcbb90a2e40fc1769762a0
Rugved Chilap <rugvedchilap@rugveds-MacBook-Pro.local>
2023-03-01 11:25:40 +0530

    added changes

--- ui/page-components/active-freight-rate-trend/components/trend-chart/index.js
+++ ui/page-components/active-freight-rate-trend/components/trend-chart/index.js
@@ -1,11 +1,10 @@
-/* eslint-disable array-callback-return */
-
-import { ResponsiveLine } from '@cogoport/charts/line';
+import { ResponsiveLine } from '@cogoport/charts';
 import { format } from '@cogoport/utils';
+
+import styles from './styles.module.css';
 
 function TrendChart({ labels, datasets = [] }) {
 	const data = [];
-
 	(datasets || []).forEach((item) => {
 		const id = item.name;
 		let temp_data = [];
@@ -16,13 +15,27 @@ function TrendChart({ labels, datasets = [] }) {
 		data.push({ id, data: temp_data });
 	});
 
+	const toolTip = (point) => {
+		const date = format(point?.data?.x, 'yyyy-MM-dd');
+		const newData = data.map((x) => ({
+			[x.id]: x.data.filter((y) => y.x === date)?.[0]?.y,
+		}));
+		return (
+			<div className={styles.styled_tip}>
+				<div className={styles.max}>{`Max - ${newData[0]?.Max}`}</div>
+				<div className={styles.min}>{`Min - ${newData[1]?.Min}`}</div>
+				<div className={styles.avg}>{`Avg - ${newData[2]?.Avg}`}</div>
+			</div>
+		);
+	};
+
 	return (
 		<div style={{ height: '400px', width: '100%' }}>
 			<ResponsiveLine
 				margin={{
-					top    : 20,
+					top    : 90,
 					right  : 0,
-					bottom : 50,
+					bottom : 40,
 					left   : 60,
 				}}
 				data={data}
@@ -35,22 +48,61 @@ function TrendChart({ labels, datasets = [] }) {
 				yScale={{
 					type: 'linear',
 				}}
+				xFormat="time:%Y-%m-%d"
 				axisLeft={{
+					orient         : 'left',
+					tickSize       : 5,
+					tickPadding    : 5,
+					tickRotation   : 0,
 					legend         : 'linear scale',
-					legendOffset   : -36,
+					legendOffset   : -40,
 					legendPosition : 'middle',
 				}}
 				axisBottom={{
 					format         : '%b %d',
+					orient         : 'bottom',
+					tickSize       : 5,
+					tickPadding    : 5,
 					tickValues     : 'every 15 days',
 					legend         : 'time scale',
 					legendOffset   : 36,
 					legendPosition : 'middle',
 				}}
+				tooltip={({ point }) => toolTip(point)}
 				enableGridY
 				enableGridX={false}
 				enablePoints={false}
 				enableSlices={false}
+				pointColor={{ theme: 'background' }}
+				pointBorderWidth={5}
+				pointBorderColor={{ from: 'serieColor' }}
+				pointLabelYOffset={-24}
+				useMesh
+				legends={[
+					{
+						anchor        : 'top-right',
+						direction     : 'column',
+						justify       : false,
+						translateX    : 18,
+						translateY    : -65,
+						itemWidth     : 100,
+						itemHeight    : 20,
+						itemsSpacing  : 4,
+						symbolSize    : 10,
+						symbolShape   : 'circle',
+						itemDirection : 'left-to-right',
+						itemTextColor : '#777',
+						effects       : [
+							{
+								on    : 'hover',
+								style : {
+									itemBackground : 'rgba(0, 0, 0, .03)',
+									itemOpacity    : 1,
+								},
+							},
+						],
+					},
+				]}
 			/>
 		</div>
 	);

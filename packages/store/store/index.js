ea452001179871e08bf5fe394a5e391182f20b25
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-03-03 15:40:07 +0530

    remove user redux store

--- packages/store/store/index.js
+++ packages/store/store/index.js
@@ -1,14 +1,6 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable no-undef */
-// import general from './general';
-// import profile from './profile';
-// import user from './user';
-
-// export default { general, profile, user };
-
-import {
-	combineReducers, createStore, applyMiddleware, compose,
-} from 'redux';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import withRedux from '../components/withRedux';
@@ -19,12 +11,16 @@ const enhancers = [];
 const middleware = [thunk];
 
 const composeWithDevTools = typeof window === 'object'
-	&& window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-	&& (process.env.NODE_ENV === 'development' || localStorage.getItem('jdgfajf7231648'))
-	? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-	: compose;
+  && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  && (process.env.NODE_ENV === 'development'
+    || localStorage.getItem('jdgfajf7231648'))
+  	? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+  	: compose;
 
-const composedEnhancers = composeWithDevTools(applyMiddleware(...middleware), ...enhancers);
+const composedEnhancers = composeWithDevTools(
+	applyMiddleware(...middleware),
+	...enhancers,
+);
 
 const createWithStore = (reducers, config) => withRedux((initialState = {}) => {
 	const rootReducer = combineReducers({ general, ...reducers });

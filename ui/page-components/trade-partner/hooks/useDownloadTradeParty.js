71675802329ea2c1da593a97a3d7ca83c00295fe
Rugved Chilap <rugvedchilap@rugveds-MacBook-Pro.local>
2023-03-15 13:11:07 +0530

    fixed windor error

--- ui/page-components/trade-partner/hooks/useDownloadTradeParty.js
+++ ui/page-components/trade-partner/hooks/useDownloadTradeParty.js
@@ -4,6 +4,7 @@ const useDownloadTradeParty = () => {
 	const { organization = '', profile = '' } = useSelector(({ s }) => s);
 
 	const downloadTradePartner = async (isArchive) => {
+		// eslint-disable-next-line no-undef
 		window.open(
 			`${process.env.BUSINESS_FINANCE_BASE_URL}
 			/saas/organization/export?organizationId=${organization?.id}

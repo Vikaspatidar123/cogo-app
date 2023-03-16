d32d8f034fa21d15a7f7b47baf365d31cd1d0628
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-03-16 18:35:22 +0530

    hs_classification

--- pages/[org_id]/[branch_id]/saas/hs-classification/index.js
+++ pages/[org_id]/[branch_id]/saas/hs-classification/index.js
@@ -1,10 +1,3 @@
-import HsClassification from '@/ui/page-components/hs-code';
-
-HsClassification.getInitialProps = () => ({
-	layout : 'app',
-	head   : {
-		title: 'Hs Classification',
-	},
-});
+import HsClassification from '@/ui/page-components/hs-code-classification';
 
 export default HsClassification;

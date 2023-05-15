9f8308495019cec54ab37b74ef41b3bfb4f3a127
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-05-15 13:49:41 +0530

    islint

--- ui/page-components/hs-code-classification/components/Header/Filters/AdvFilters/index.js
+++ ui/page-components/hs-code-classification/components/Header/Filters/AdvFilters/index.js
@@ -90,7 +90,7 @@ function AdvFilters({
 						type="button"
 						disabled={loading}
 						onClick={() => {
-                        	clearFilterHandler();
+							clearFilterHandler();
 						}}
 					>
 						Clear Filter

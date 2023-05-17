c5e6b7fcf1ec1e7a40476eba001b29d52ca101ef
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-05-17 13:05:52 +0530

    fix

--- ui/page-components/trade-partner/hooks/useList.js
+++ ui/page-components/trade-partner/hooks/useList.js
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Toast } from '@cogoport/components';
 import { useState, useEffect } from 'react';
 
 import { useRequestBf } from '@/packages/request';

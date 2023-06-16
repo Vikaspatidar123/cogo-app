4238adbc8adae928b6a8bf1ed17b3ffcb0106c89
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-06-16 16:45:56 +0530

    eslint fix

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -9,16 +9,16 @@ const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 const IS_PROD = process.env.NEXT_PUBLIC_NODE_ENV === 'production';
 
 if (IS_PROD && SENTRY_DSN) {
-    init({
-        dsn: SENTRY_DSN,
-        environment: process.env.NEXT_PUBLIC_NODE_ENV,
-        integrations: [new BrowserTracing()],
-        // Adjust this value in production, or use tracesSampler for greater control
-        tracesSampleRate: 1.0,
-        // tracesSampleRate: IS_PROD ? 0.2 : 1.0,
-        // ...
-        // Note: if you want to override the automatic release value, do not set a
-        // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-        // that it will also get attached to your source maps
-    });
+	init({
+		dsn              : SENTRY_DSN,
+		environment      : process.env.NEXT_PUBLIC_NODE_ENV,
+		integrations     : [new BrowserTracing()],
+		// Adjust this value in production, or use tracesSampler for greater control
+		tracesSampleRate : 1.0,
+		// tracesSampleRate: IS_PROD ? 0.2 : 1.0,
+		// ...
+		// Note: if you want to override the automatic release value, do not set a
+		// `release` value here - use the environment variable `SENTRY_RELEASE`, so
+		// that it will also get attached to your source maps
+	});
 }

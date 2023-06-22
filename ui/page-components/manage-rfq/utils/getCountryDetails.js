e81589d2b5a5efbcf4327b5898c0209ec51d5c77
shivanibairoju <shivani.bairoju@cogoport.com>
2023-06-22 13:26:09 +0530

    counties

--- ui/page-components/manage-rfq/utils/getCountryDetails.js
+++ ui/page-components/manage-rfq/utils/getCountryDetails.js
@@ -1,6 +1,6 @@
 import COUNTRIES from '@/.data-store/constants/countries.json';
 
-const countriesHash = COUNTRIES.reduce(
+const countriesHash = (COUNTRIES || []).reduce(
 	(pv, acc) => ({ ...pv, [acc.id]: acc }),
 	{},
 );

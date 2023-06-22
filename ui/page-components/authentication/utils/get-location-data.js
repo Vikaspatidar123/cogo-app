f9605d1f2da9f730047799d7650b27a8c69da257
anmolb-cogo <anmol.bansal@cogoport.com>
2023-06-22 12:34:54 +0530

    api key addition in comments

--- ui/page-components/authentication/utils/get-location-data.js
+++ ui/page-components/authentication/utils/get-location-data.js
@@ -1,7 +1,14 @@
 const BASE_URL = process.env.NEXT_PUBLIC_USER_LOCATION_API_URL;
+// const API_KEY = process.env.NEXT_PUBLIC_IP_DATA_API_KEY;
 
 export const getLocationData = async () => {
-	const response = await fetch(BASE_URL);
+	const url = BASE_URL;
+
+	// if (API_KEY) {
+	// 	url = `${BASE_URL}?api-key=${API_KEY}`;
+	// }
+
+	const response = await fetch(url);
 
 	const data = await response.json();
 

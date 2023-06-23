1d67dd65bbe7cd660429af6b585f3bbeb3de70f6
anmolb-cogo <anmol.bansal@cogoport.com>
2023-06-23 16:45:28 +0530

    fix

--- packages/forms/Business/SelectCountryCode/index.js
+++ packages/forms/Business/SelectCountryCode/index.js
@@ -1,34 +1,35 @@
 /* eslint-disable import/no-unresolved */
 import { Select } from '@cogoport/components';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 import countryCode from '@/.data-store/constants/countries.json';
 
 function SelectCountryCode(props) {
-	const { value, showCountryName = true, height = 18, width = 18 } = props;
+	// const { value, showCountryName = true, height = 18, width = 18 } = props;
+	const { value } = props;
 
 	const formattedList = countryCode.map((code) => ({
 		id    : code.id,
 		value : code.mobile_country_code,
 
-		// label: `${code.mobile_country_code || ''}: ${code.name || ''}`,
+		label: `${code.mobile_country_code || ''}: ${code.name || ''}`,
 
-		label: (
-			<div style={{ display: 'flex', alignItems: 'center' }}>
-				<span style={{ marginRight: '5px' }}>
-					<Image
-						width={width}
-						height={height}
-						src={code.flag_icon_url ? code.flag_icon_url : 'https://via.placeholder.com/24x20'}
-						alt={code.name}
-					/>
-				</span>
-				<span style={{ marginTop: -4 }}>
-					{code.mobile_country_code}
-					{showCountryName && `: ${code.name}`}
-				</span>
-			</div>
-		),
+		// label: (
+		// 	<div style={{ display: 'flex', alignItems: 'center' }}>
+		// 		<span style={{ marginRight: '5px' }}>
+		// 			<Image
+		// 				width={width}
+		// 				height={height}
+		// 				src={code.flag_icon_url ? code.flag_icon_url : 'https://via.placeholder.com/24x20'}
+		// 				alt={code.name}
+		// 			/>
+		// 		</span>
+		// 		<span style={{ marginTop: -4 }}>
+		// 			{code.mobile_country_code}
+		// 			{showCountryName && `: ${code.name}`}
+		// 		</span>
+		// 	</div>
+		// ),
 
 	}));
 

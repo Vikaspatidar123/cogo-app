285ad249662c738c49df714ec2479387cf793aa5
Vansh Jaiswal <vansh@Vanshs-MacBook-Air.local>
2023-06-23 14:23:56 +0530

    pr changes fix

--- pages/raise-cancellation-ticket.js
+++ pages/raise-cancellation-ticket.js
@@ -1,14 +1,3 @@
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-
 import Unsubscribe from '@/ui/page-components/unsubscribe-page';
-
-export async function getServerSideProps({ locale }) {
-	return {
-		props: {
-			...(await serverSideTranslations(locale, ['common'])),
-
-		},
-	};
-}
 
 export default Unsubscribe;

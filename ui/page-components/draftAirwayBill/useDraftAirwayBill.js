490cc32d0e233933e29aee200f51fe8360524909
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-06-27 19:16:03 +0530

    change api call

--- ui/page-components/draftAirwayBill/useDraftAirwayBill.js
+++ ui/page-components/draftAirwayBill/useDraftAirwayBill.js
@@ -1,5 +1,5 @@
 import { request } from '@/packages/request/helpers/request';
 
-const useDraftAirwayBill = (body) => request.app.post('/verify_awb_email_token', body);
+const useDraftAirwayBill = (body) => request.post('/verify_awb_email_token', body);
 
 export default useDraftAirwayBill;

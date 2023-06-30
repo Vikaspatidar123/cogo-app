83312ddfdb289ca5ccefb120a0e443fd4b0d7257
shivanibairoju <shivani.bairoju@cogoport.com>
2023-06-30 16:44:26 +0530

    api removed

--- packages/navigation-configs/apis/chat-bot-apis.js
+++ packages/navigation-configs/apis/chat-bot-apis.js
@@ -4,11 +4,6 @@ const chat_bot_apis = [
 		access_type  : 'private',
 		service_name : 'communication',
 	},
-	{
-		api:
-		'https://cogoport-testing.sgp1.digitaloceanspaces.com/b3949cf1f8cd3366d0272bd60c87c930/emoji-list.json',
-		access_type: 'public',
-	},
 ];
 
 export default chat_bot_apis;

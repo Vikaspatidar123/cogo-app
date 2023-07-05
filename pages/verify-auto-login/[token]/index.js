cb0a87d836e380ee9bce62ac5247fa00c61bfc43
shivanibairoju <shivani.bairoju@cogoport.com>
2023-07-05 16:41:40 +0530

    pr changes

--- pages/verify-auto-login/[token]/index.js
+++ pages/verify-auto-login/[token]/index.js
@@ -1,8 +1,7 @@
 import { Toast } from '@cogoport/components';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-import { AUTO_LOGIN_PAGE_MAPPINGS as PAGE_MAPPINGS } from '@/ui/commons/constants/pageMappings';
-import setCookieAndRedirect from '@/ui/commons/utils/setCookieAndRedirect';
+import getRedirectionUrlForAutoLoginSignup from '@/ui/helpers/getRedirectionUrlForAutoLoginSignup';
 import VerifyAutoLogin from '@/ui/page-components/authentication/components/VerifyAutoLogin';
 import getVerifyAutoLogin from '@/ui/page-components/authentication/hooks/useVerifyAutoLogin';
 
@@ -22,40 +21,7 @@ export async function getServerSideProps(ctx) {
 			lead_organization_id,
 			lead_action_id,
 		});
-		const { hasError } = response || {};
-
-		if (!hasError) {
-			const {
-				token,
-				lead_action = {},
-				organization_id,
-				organization_branch_id,
-				user_id,
-			} = (response || {}).data || {};
-
-			const redirect_page = lead_action?.action_config?.redirect_page || '';
-
-			let uri = `/${organization_id}/${organization_branch_id}/${PAGE_MAPPINGS?.[redirect_page]}`;
-			if (redirect_page === 'search_results') {
-				const queryObj = {
-					importer_exporter_id        : organization_id,
-					importer_exporter_branch_id : organization_branch_id,
-					user_id,
-					lead_action_id,
-				};
-
-				uri = `${uri}?query=${JSON.stringify(queryObj)}`;
-			}
-
-			if (
-				['app_dashboard', 'app_discover_rates'].includes(redirect_page)
-                && lead_action?.action_config?.search_mode === 'generic_search'
-			) {
-				uri = `${uri}?service_type=${lead_action?.action_config?.service_type}`;
-			}
-
-			setCookieAndRedirect(token, ctx, uri);
-		}
+		getRedirectionUrlForAutoLoginSignup({ response, lead_action_id, ctx, actionType: 'autoLogin' });
 	} catch (e) {
 		Toast.error('Something went wrong, we are working on it!');
 	}
--- pages/verify-auto-sign-up-email/[token]/index.js
+++ pages/verify-auto-sign-up-email/[token]/index.js
@@ -1,7 +1,6 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-import { AUTO_SIGNUP_PAGE_MAPPINGS as PAGE_MAPPINGS } from '@/ui/commons/constants/pageMappings';
-import setCookieAndRedirect from '@/ui/commons/utils/setCookieAndRedirect';
+import getRedirectionUrlForAutoLoginSignup from '@/ui/helpers/getRedirectionUrlForAutoLoginSignup';
 import getVerifyAutoSignUpEmail from '@/ui/page-components/authentication/hooks/useVerifyAutoSignUpEmail';
 import { VerifyAutoSignUpEmail } from '@/ui/page-components/authentication/index';
 
@@ -22,40 +21,7 @@ export async function getServerSideProps(ctx) {
 			lead_action_id,
 		});
 
-		const { hasError } = response || {};
-
-		if (!hasError) {
-			const {
-				token,
-				lead_action = {},
-				organization_id,
-				organization_branch_id,
-				user_id,
-			} = (response || {}).data || {};
-
-			const redirect_page = lead_action?.action_config?.redirect_page || '';
-
-			let uri = `/${organization_id}/${organization_branch_id}/${PAGE_MAPPINGS?.[redirect_page]}`;
-			if (redirect_page === 'search_results') {
-				const queryObj = {
-					importer_exporter_id        : organization_id,
-					importer_exporter_branch_id : organization_branch_id,
-					user_id,
-					lead_action_id,
-				};
-
-				uri = `${uri}?query=${JSON.stringify(queryObj)}`;
-			}
-
-			if (
-				['app_dashboard', 'app_discover_rates'].includes(redirect_page)
-        && lead_action?.action_config?.search_mode === 'generic_search'
-			) {
-				uri = `${uri}?service_type=${lead_action?.action_config?.service_type}`;
-			}
-
-			setCookieAndRedirect(token, ctx, uri);
-		}
+		getRedirectionUrlForAutoLoginSignup({ response, lead_action_id, ctx, actionType: 'autoSignUp' });
 	} catch (e) {
 		console.error(e.toString());
 	}

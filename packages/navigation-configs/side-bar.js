2a231ed9141632116ce47c5fb60c792a7a5fbcbd
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-07-07 16:01:35 +0530

    eslint changes

--- packages/navigation-configs/side-bar.js
+++ packages/navigation-configs/side-bar.js
@@ -1,5 +1,7 @@
 import navigationMappings from './navigation-mapping';
+
 import GLOBAL_CONSTANTS from '@/ui/commons/constants/globals';
+
 const getCondition = (urlItem) => {
 	const condition = {};
 	if (urlItem?.user_email) {
@@ -44,11 +46,11 @@ const getSideBarConfigs = ({
 					nav_items.push({
 						...navigation[key],
 						options: dashboardUrls.map((urlItem) => ({
-							title: urlItem.title,
-							type: 'link',
-							as: `/dashboards/${urlItem.urlKey}`,
-							href: '/dashboards/[dashboard_type]',
-							condition: getCondition(urlItem),
+							title     : urlItem.title,
+							type      : 'link',
+							as        : `/dashboards/${urlItem.urlKey}`,
+							href      : '/dashboards/[dashboard_type]',
+							condition : getCondition(urlItem),
 						})),
 					});
 				} else if (navigation[key]?.options) {

ce6514a019782b6216633abddaa03248e774f0cb
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-07-07 12:57:44 +0530

    move ied to common

--- ui/commons/components/ImportExportDoc/index.js
+++ ui/commons/components/ImportExportDoc/index.js
@@ -1,5 +1,87 @@
-const ImportExportDoc = ({ documents }) => {
+import { TabPanel, Tabs, cl } from '@cogoport/components';
+import { isEmpty } from '@cogoport/utils';
+import { useState, useEffect } from 'react';
 
-};
+import Documents from './Documents';
+import styles from './styles.module.css';
+
+function ImportExportDoc({ documents = [], EmptyState, hsNumber, modeOfTransport, isModal = false }) {
+	const [activeTab, setActiveTab] = useState('IMPORT');
+
+	const [docVal, setDocVal] = useState({
+		importDocs : [],
+		exportDocs : [],
+	});
+
+	const TAB_MAPPING = {
+		IMPORT : docVal.importDocs,
+		EXPORT : docVal.exportDocs,
+	};
+
+	useEffect(() => {
+		if (!isEmpty(documents)) {
+			const impDoc = [];
+			const expDoc = [];
+
+			documents.forEach((doc) => {
+				if (doc?.tradeType === 'IMPORT') {
+					impDoc.push(doc);
+				} else {
+					expDoc.push(doc);
+				}
+			});
+
+			setDocVal({
+				importDocs : impDoc,
+				exportDocs : expDoc,
+			});
+		}
+	}, [documents]);
+
+	return (
+		<>
+			<div className={styles.flex_box}>
+				<div className={styles.tabs}>
+					<Tabs
+						activeTab={activeTab}
+						themeType="tertiary"
+						onChange={setActiveTab}
+					>
+						{/* <TabPanel name="IMPORT" title={t('importExportDoc:result_tab_1')} />
+					<TabPanel name="EXPORT" title={t('importExportDoc:result_tab_2')} /> */}
+						<TabPanel name="IMPORT" title="Import" />
+						<TabPanel name="EXPORT" title="Export" />
+					</Tabs>
+				</div>
+				<div className={styles.tag_container}>
+					<div className={cl`${styles.tag} ${styles.transport_mode}`}>
+						{/* {t('importExportDoc:document_control_transport_label')} */}
+						Mode of Transport:
+						:
+						{' '}
+						{modeOfTransport}
+					</div>
+					{hsNumber && (
+						<div className={styles.tag}>
+							{/* {t('importExportDoc:document_control_hscode_label')} */}
+							Hs Code:
+							:
+							{' '}
+							{hsNumber}
+						</div>
+					)}
+				</div>
+			</div>
+			<div className={cl`${styles.doc_container} ${isModal ? styles.scroll : ''}`}>
+				{!isEmpty(TAB_MAPPING[activeTab])
+					? TAB_MAPPING[activeTab].map((doc, index) => (
+						<Documents key={`${doc?.docLink}_${index + 1}`} doc={doc} hsNumber={hsNumber} />
+					))
+					: <EmptyState />}
+			</div>
+
+		</>
+	);
+}
 
 export default ImportExportDoc;
--- ui/page-components/order-history/common/DetailsModal/IEDocumentsModal/index.js
+++ ui/page-components/order-history/common/DetailsModal/IEDocumentsModal/index.js
@@ -1,80 +1,27 @@
-import { Modal, TabPanel, Tabs } from '@cogoport/components';
 import { isEmpty } from '@cogoport/utils';
-import { useMemo, useState } from 'react';
 
 import EmptyState from '../../EmptyState';
 
-import Document from './Document';
 import styles from './styles.module.css';
 
-const importDoc = [];
-const exportDoc = [];
-
-const TAB_MAPPING = {
-	IMPORT : importDoc,
-	EXPORT : exportDoc,
-};
+import DocumentResult from '@/ui/commons/components/ImportExportDoc';
 
 function IEDocumentsModal({ tradeEngineResponse = {} }) {
-	const [activeTab, setActiveTab] = useState('IMPORT');
-
 	const { modeOfTransport = '', lineItem = [] } = tradeEngineResponse;
 	const { hsNumber = '', documents = [] } = lineItem?.[0] || {};
-
-	useMemo(() => {
-		(documents || []).forEach((doc) => {
-			if (doc?.tradeType === 'IMPORT') {
-				importDoc.push(doc);
-			} else {
-				exportDoc.push(doc);
-			}
-		});
-	}, [documents]);
 
 	return (
 		<div className={styles.container}>
 			{!isEmpty(documents) ? (
-				<>
-					<div className={styles.modal_header}>
-						<div className={styles.tabs}>
-							<Tabs
-								activeTab={activeTab}
-								themeType="primary"
-								onChange={setActiveTab}
-							>
-								<TabPanel name="IMPORT" title="Import" />
-								<TabPanel name="EXPORT" title="Export" />
-							</Tabs>
-						</div>
-						<div className={styles.tag_container}>
-							<div
-								className={`${styles.tag} ${styles.transport_mode}`}
-							>
-								Mode of Transport:
-								{' '}
-								{modeOfTransport}
-							</div>
-							<div className={styles.tag}>
-								Hs Code:
-								{' '}
-								{hsNumber}
-							</div>
-						</div>
-					</div>
-					<Modal.Body>
-						<div>
-							<div>
-								{isEmpty(TAB_MAPPING?.[activeTab]) ? (
-									<EmptyState />
-								) : (
-									TAB_MAPPING?.[activeTab].map((doc) => (
-										<Document key={doc?.docLink} doc={doc} hsNumber={hsNumber} />
-									))
-								)}
-							</div>
-						</div>
-					</Modal.Body>
-				</>
+				<div>
+					<DocumentResult
+						documents={documents}
+						hsNumber={hsNumber}
+						modeOfTransport={modeOfTransport}
+						EmptyState={EmptyState}
+						isModal
+					/>
+				</div>
 			) : (
 				<EmptyState />
 			)}

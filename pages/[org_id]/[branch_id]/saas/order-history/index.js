cfb8b6829d91e3458b3a22a1e32b3a971dfe0cfa
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-07-11 11:04:59 +0530

    added dt and tec from common

--- pages/[org_id]/[branch_id]/saas/order-history/index.js
+++ pages/[org_id]/[branch_id]/saas/order-history/index.js
@@ -5,7 +5,8 @@ import OrderHistory from '@/ui/page-components/order-history';
 export async function getServerSideProps({ locale }) {
 	return {
 		props: {
-			...(await serverSideTranslations(locale, ['common', 'orderHistory', 'iecResult', 'iedResult'])),
+			...(await serverSideTranslations(locale, ['common', 'orderHistory',
+				'iecResult', 'iedResult', 'dtResult', 'tecResult'])),
 
 		},
 	};

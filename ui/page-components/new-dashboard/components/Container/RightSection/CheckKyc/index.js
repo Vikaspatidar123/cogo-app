0c15bc07782c484906091bb9bb8f4688b4f4c890
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-07-11 11:29:30 +0530

    changes

--- ui/page-components/new-dashboard/components/Container/RightSection/CheckKyc/index.js
+++ ui/page-components/new-dashboard/components/Container/RightSection/CheckKyc/index.js
@@ -1,16 +1,33 @@
-import { Button } from '@cogoport/components'
-import styles from './styles.module.css'
-import { IcAInsurance, IcMArrowNext } from '@cogoport/icons-react'
-const CheckKyc = () => {
-    return <div className={styles.container}>
-        <div className={styles.box}>
-            <IcAInsurance width={50} height={50} />
-            <div>
-                <div className={styles.heading}>KYC Verification is pending!</div>
-                <div className={styles.des}>Kindly upload the required documents to proceed further.</div>
-                <Button size='sm' themeType="accent" type='button'>Complete KYC <IcMArrowNext /></Button>
-            </div>
-        </div>
-    </div>
+import { Button } from '@cogoport/components';
+import { IcMArrowNext } from '@cogoport/icons-react';
+
+import styles from './styles.module.css';
+
+import { Image } from '@/packages/next';
+
+function CheckKyc() {
+	return (
+		<div className={styles.container}>
+			<div className={styles.box}>
+				<Image
+					src="https://cdn.cogoport.io/cms-prod/cogo_app/vault/original/Data_security_14-removebg-preview.svg"
+					alt="Kyc"
+					width={60}
+					height={60}
+				/>
+				<div>
+					<div className={styles.heading}>KYC Verification is pending!</div>
+					<div className={styles.des}>Kindly upload the required documents to proceed further.</div>
+					<div className={styles.button}>
+						<Button size="sm" themeType="linkUi" type="button">
+							Complete KYC
+							<IcMArrowNext />
+						</Button>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	);
 }
-export default CheckKyc
+export default CheckKyc;

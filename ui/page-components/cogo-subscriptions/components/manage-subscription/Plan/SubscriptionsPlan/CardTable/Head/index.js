86fc8b019676aff7fd0ae219a615a568f0135c2d
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-07-13 19:23:49 +0530

    pr changes

--- ui/page-components/cogo-subscriptions/components/manage-subscription/Plan/SubscriptionsPlan/CardTable/Head/index.js
+++ ui/page-components/cogo-subscriptions/components/manage-subscription/Plan/SubscriptionsPlan/CardTable/Head/index.js
@@ -14,7 +14,7 @@ function Head({ check }) {
 
 	return (
 		<div className={cl`${styles.container} ${check && styles.check}`}>
-			<div className={cl`${styles.col} ${styles.feature} ${styles.featureTitle}`} width="30%" />
+			<div className={cl`${styles.col} ${styles.feature} ${styles.feature_title}`} />
 			<div className={styles.plan_name}>
 				{(SORTED_PlAN || []).map((display_name) => (
 					<div className={cl`${styles.col} ${styles.text}`} key={display_name}>

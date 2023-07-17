59833873a8070c5abff450f9825bc1eea66d6b27
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-07-17 11:04:04 +0530

    change img tag

--- ui/page-components/create-ticket/components/EmptyState/index.js
+++ ui/page-components/create-ticket/components/EmptyState/index.js
@@ -3,6 +3,7 @@ import { useTranslation } from 'next-i18next';
 
 import styles from './styles.module.css';
 
+import { Image } from '@/packages/next';
 import GLOBAL_CONSTANTS from '@/ui/commons/constants/globals';
 
 function EmptyState({ containerHeight = '' }) {
@@ -11,7 +12,7 @@ function EmptyState({ containerHeight = '' }) {
 		<div className={styles.empty_card}>
 			<div className={styles.container}>
 				<div className={cl`${styles.wrapper} ${containerHeight ? styles.wrapper_height : ''}`}>
-					<img
+					<Image
 						src={GLOBAL_CONSTANTS.image_url.create_ticket_empty_state}
 						alt={t('createTicketPublic:empty_state')}
 						height={360}

126646ff30081e2684048b1df130652119205083
Harshitcogo <harshit.soni@cogoport.com>
2023-07-17 15:33:40 +0530

    fixes

--- ui/commons/components/PasswordValidator/index.js
+++ ui/commons/components/PasswordValidator/index.js
@@ -1,32 +1,8 @@
-/* eslint-disable no-unused-vars */
 import { IcCTick } from '@cogoport/icons-react';
 
+import VALID_PASSWORD_MAPPINGS from '../../utils/getValidPasswordMapping';
+
 import styles from './styles.module.css';
-
-import patterns from '@/ui/commons/configurations/patterns';
-
-const VALID_PASSWORD_MAPPINGS = {
-	lowercase: {
-		pattern : patterns.PASSWORD.lowercase,
-		message : 'at least one lowercase character.',
-	},
-	uppercase: {
-		pattern : patterns.PASSWORD.uppercase,
-		message : 'at least one uppercase character.',
-	},
-	digit: {
-		pattern : patterns.PASSWORD.digit,
-		message : 'at least one digit.',
-	},
-	special: {
-		pattern : patterns.PASSWORD.special,
-		message : 'at least one special character (!@#$%^&*).',
-	},
-	minLength: {
-		pattern : patterns.PASSWORD.minLength,
-		message : 'minimum 8 characters.',
-	},
-};
 
 function PasswordValidator({ password = '' }) {
 	return (
@@ -35,13 +11,19 @@ function PasswordValidator({ password = '' }) {
 
 			<div className={styles.list}>
 				{Object.entries(VALID_PASSWORD_MAPPINGS)?.map(([key, value]) => {
-					const { pattern = null, message = '' } = value;
-					const regex = new RegExp(pattern);
-					const isValid = regex.test(password);
+					const { message = '', length = 0, characters = [] } = value;
+
+					let isValid = false;
+					if (key === 'minLength') {
+						isValid = password.length >= length;
+					} else {
+						isValid = password.split('').some((char) => characters.includes(char));
+					}
+
 					return (
-						<div className={styles.item}>
-							<div className={`${styles.icon} `}>
-								{isValid ? <IcCTick /> : <div className={styles.dot} />}
+						<div key={key} className={styles.item}>
+							<div className={styles.icon}>
+								{isValid ? <IcCTick width={12} height={16} /> : <div className={styles.dot} />}
 							</div>
 
 							<div className={styles.message}>{message}</div>

b68b75d14beb07b7bde75b024b74b43c987c893e
Harshitcogo <harshit.soni@cogoport.com>
2023-07-18 17:21:12 +0530

    fix

--- ui/commons/utils/getValidPasswordMapping.js
+++ ui/commons/utils/getValidPasswordMapping.js
@@ -13,24 +13,24 @@ function rangeOfChars(start, stop) {
 
 const VALID_PASSWORD_MAPPINGS = ({ t }) => ({
 	lowercase: {
-		characters: rangeOfChars('a', 'z'),
-		message: t('common:password_validator_rule_message_1'),
+		characters : rangeOfChars('a', 'z'),
+		message    : t('common:password_validator_rule_message_1'),
 	},
 	uppercase: {
-		characters: rangeOfChars('A', 'Z'),
-		message: t('common:password_validator_rule_message_2'),
+		characters : rangeOfChars('A', 'Z'),
+		message    : t('common:password_validator_rule_message_2'),
 	},
 	digit: {
-		characters: rangeOfChars('0', '9'),
-		message: t('common:password_validator_rule_message_3'),
+		characters : rangeOfChars('0', '9'),
+		message    : t('common:password_validator_rule_message_3'),
 	},
 	special: {
-		characters: '!@#$%^&*'.split(''),
-		message: t('common:password_validator_rule_message_4'),
+		characters : '!@#$%^&*'.split(''),
+		message    : t('common:password_validator_rule_message_4'),
 	},
 	minLength: {
-		length: 8,
-		message: t('common:password_validator_rule_message_5'),
+		length  : 8,
+		message : t('common:password_validator_rule_message_5'),
 	},
 });
 

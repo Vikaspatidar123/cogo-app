b073d266000554438e1d08d8f6785a88b16c9f94
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-07-18 18:53:00 +0530

    pr changes

--- ui/page-components/dashboard/hooks/useGetTracking.js
+++ ui/page-components/dashboard/hooks/useGetTracking.js
@@ -3,6 +3,10 @@ import { useEffect, useCallback } from 'react';
 import { useRequest } from '@/packages/request';
 import { useSelector } from '@/packages/store';
 
+const getPayload = ({ orgId }) => ({
+	organization: orgId,
+});
+
 function useGetTracking() {
 	const { kyc_status, organizationId } = useSelector(({ profile }) => ({
 		kyc_status     : profile?.organization?.kyc_status,
@@ -19,19 +23,15 @@ function useGetTracking() {
 		container_tracking: oceanTracking = {},
 	} = data?.dashboard_products || {};
 
-	const getPayload = useCallback(() => ({
-		organization: organizationId,
-	}), [organizationId]);
-
 	const getDashboardTrackingInfo = useCallback(() => {
 		try {
 			trigger({
-				params: getPayload(),
+				params: getPayload({ orgId: organizationId }),
 			});
 		} catch (err) {
 			console.error(err?.message || ' Please try again.');
 		}
-	}, [getPayload, trigger]);
+	}, [organizationId, trigger]);
 
 	useEffect(() => {
 		getDashboardTrackingInfo();

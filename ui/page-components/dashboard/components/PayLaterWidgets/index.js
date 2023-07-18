1e8d27f5e049102ebcaa6d55d674d80fd1bff8bb
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-07-18 20:09:58 +0530

    fixes

--- ui/page-components/dashboard/components/PayLaterWidgets/index.js
+++ ui/page-components/dashboard/components/PayLaterWidgets/index.js
@@ -1,4 +1,5 @@
 import { Button } from '@cogoport/components';
+import { useTranslation } from 'next-i18next';
 
 import styles from './styles.module.css';
 
@@ -6,35 +7,35 @@ import { useRouter } from '@/packages/next';
 
 function PayLaterWidgets() {
 	const { push } = useRouter();
+	const { t } = useTranslation(['dashboard']);
 
 	return (
-		<div className={styles.container}>
-			<div className={styles.IconContainer}>
-				<img src="https://cdn.cogoport.io/cms-prod/cogo_app/vault/original/air.svg" alt="new" />
+		<div className={styles.header}>
+			<div className={styles.body}>
+				{t('dashboard:pay_later_text_1')}
+				<span className={styles.blue}>{t('dashboard:pay_later_text')}</span>
+				{t('dashboard:pay_later_text_2')}
 			</div>
-			<div className={styles.Header}>
-				Ship Now,
-				<span className="blue">Pay Later </span>
-				Easily apply online in just a few minutes
-			</div>
+			<div className={styles.mainBody}>
+				<div className={styles.sub}>
+					<img
+						className={styles.img}
+						src="https://cdn.cogoport.io/cms-prod/cogo_app/vault/original/newadd.svg"
+						alt="img"
+					/>
 
-			<div className={styles.InfoContainer}>
-				<div className={styles.SvgContainer}>
-					<img src="https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/docx.svg" alt="doc" />
 				</div>
-				<div className={styles.text} size={12}>
-					Pay up to 90 days later for shipments, local transport, and other
-					trade services without additional charges.
-				</div>
-			</div>
+				<p className={styles.text}>
+					{t('dashboard:pay_later_text_3')}
+				</p>
 
-			<div className={styles.ButtonContainer}>
-				<Button themeType="secondary" onClick={() => push('/pay-later')}>
-					CHECK ELIGIBILITY NOW
+			</div>
+			<div className={styles.Bottom}>
+				<Button className={styles.button} type="button" onClick={() => push('/pay-later')}>
+					{t('dashboard:pay_later_text_4')}
 				</Button>
 			</div>
 		</div>
 	);
 }
-
 export default PayLaterWidgets;

bc3cd74ed50b34ee857bec56e9fa0252b65b19ed
Kanchan Bhandari <kanchan.bhandari@cogoport.com>
2023-07-19 17:05:38 +0530

    changes in sample file

--- ui/page-components/documents/constants/checkFileList.js
+++ ui/page-components/documents/constants/checkFileList.js
@@ -15,23 +15,22 @@ export const checkFileList = [
 	{
 		doc_name    : 'IEC copy (signed and stamp) ',
 		doc_type    : 'kyc_docs',
-		sample_link : 'https://cogofreight-my.sharepoint.com/personal/aman_kumar_cogoport_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments%2FIEC%5Fformat%2Epng&parent=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments&ga=1',
+		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/c8cfaa65c6d3e59b38d13e0900985e18/IEC_format%20%281%29.png',
 	},
 	{
 		doc_name    : 'Gst registration (signed and stamp) ',
 		doc_type    : 'company_tax_docs',
-		sample_link : 'https://cogofreight-my.sharepoint.com/personal/aman_kumar_cogoport_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments%2FGST%2DRegistration%2DCertificate%2DSample%2DAnnexure%2DA%2Epng&parent=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments&ga=1',
+		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/95c72c88842372e66c57bbdf221ebeb5/GST-Registration-Certificate-Sample-Annexure-A%20%281%29.png',
 	},
 	{
 		doc_name    : 'Certificate of incorporation',
 		doc_type    : 'company_registration_docs',
-		sample_link : 'https://cogofreight-my.sharepoint.com/personal/aman_kumar_cogoport_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Faman_kumar_cogoport_com%2FDocuments%2FCertificate-Of-Incorporation%2Ejpeg&parent=%2Fpersonal%2Faman_kumar_cogoport_com%2FDocuments&ga=1',
+		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/b0e35c0aa25953e5e08e5644cd396420/Certificate-Of-Incorporation.jpeg',
 	},
-
 	{
 		doc_name    : 'Memorandum & Article of association ',
 		doc_type    : 'company_registration_docs',
-		sample_link : 'https://cogofreight-my.sharepoint.com/personal/aman_kumar_cogoport_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments%2FMOA%2DAOA%5Fsample%20template%2Epng&parent=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments&ga=1',
+		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/02bdaad19978dbb68f367485bfe19bf2/MOA-AOA_sample%20template%20%281%29.png',
 	},
 	{
 		doc_name : 'Self-attested company/LLP master data.',
@@ -40,23 +39,22 @@ export const checkFileList = [
 	{
 		doc_name    : 'Bank Authorization Letter on Bank\'s letterhead duly signed & stamp',
 		doc_type    : 'company_financial_docs',
-		sample_link : 'https://cogofreight-my.sharepoint.com/personal/abhijeet_thakur_cogoport_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fabhijeet%5Fthakur%5Fcogoport%5Fcom%2FDocuments%2FMicrosoft%20Teams%20Chat%20Files%2FBank%20Authorisation%20Letter%20%2D%20New%2Epdf&parent=%2Fpersonal%2Fabhijeet%5Fthakur%5Fcogoport%5Fcom%2FDocuments%2FMicrosoft%20Teams%20Chat%20Files&ga=1',
+		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/c49dce5b0e71e0c3d8845b007e46eb9b/Bank%20Authorisation%20Letter%20-%20New%20%281%29.pdf',
 	},
-
 	{
 		doc_name    : 'GST Return form-3B of Last 3 months(signed and stamped)',
 		doc_type    : 'company_tax_docs',
-		sample_link : 'https://cogofreight-my.sharepoint.com/personal/aman_kumar_cogoport_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments%2FGSTR%2D3B%2DReturn%2DFormat%2Epng&parent=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments&ga=1',
+		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/dc278220e954a2c54cf65be2684c1e23/GSTR-3B-Return-Format%20%281%29.png',
 	},
 	{
 		doc_name    : 'Original Authorization Letter to CHA(original) duly signed and stamped',
-		doc_type    : 'company_gen_docs',
-		sample_link : 'https://cogofreight-my.sharepoint.com/personal/aman_kumar_cogoport_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments%2FCHA%20authority%20letter%20format%20copy%2Etiff&parent=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments&ga=1',
+		doc_type    : 'company_general_docs',
+		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/a554c3cdb8e16a1c9de7fa18e95959ff/CHA%20authority%20letter%20format%20copy%20%281%29.tiff',
 	},
 	{
 		doc_name    : 'KYC letter on customer\'s letterhead duly signed &stamped',
 		doc_type    : 'kyc_docs',
-		sample_link : 'https://cogofreight-my.sharepoint.com/personal/aman_kumar_cogoport_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments%2FKYC%20%281%29%2Epdf&parent=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments&ga=1',
+		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/739799d6ba4ee67a4098765d32a67058/KYC%20%281%29%20%281%29.pdf',
 	},
 	{
 		doc_name : 'ITR of last 2 years of firm / Propreitor/Directors',
@@ -69,7 +67,7 @@ export const checkFileList = [
 	{
 		doc_name    : 'Board Resolution if company',
 		doc_type    : 'company_registration_docs',
-		sample_link : 'https://cogofreight-my.sharepoint.com/personal/aman_kumar_cogoport_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments%2FTemplate%2DNonGovt%2DBoardResolution%2D2021%5Fpage%2D0001%2D724x1024%2Ejpeg&parent=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments&ga=1',
+		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/b65deadb0adb1a7216ff06ab13ab2161/Template-NonGovt-BoardResolution-2021_page-0001-724x1024%20%281%29.jpeg',
 	},
 	{
 		doc_name : 'Partnership deed or certificate of incorporation if LLP orIndividual Owner',
@@ -81,8 +79,8 @@ export const checkFileList = [
 	},
 	{
 		doc_name    : 'Request Letter Addressed to Assistant/DeputyCommissioner of Custom at Desired Port',
-		doc_type    : 'company_gen_docs',
-		sample_link : 'https://cogofreight-my.sharepoint.com/:w:/g/personal/abhijeet_thakur_cogoport_com/Ecw1DWAM479BsDO_0aHxGhIBqcDHE9PzWlq2FFMZGq4iUQ?e=E9Doj5',
+		doc_type    : 'company_general_docs',
+		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/ab637cead9e949aada5115faf89871b4/Covering%20Letter-Request%20letter%20to%20Assis%3Adeputy%20commissionar.doc',
 	},
 	{
 		doc_name : 'Bank Statement of last 6 Months ',
@@ -95,7 +93,7 @@ export const checkFileList = [
 	{
 		doc_name    : 'Original AD code letter From Bank',
 		doc_type    : 'company_financial_docs',
-		sample_link : 'https://cogofreight-my.sharepoint.com/personal/aman_kumar_cogoport_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments%2Fauthorized%2Ddealership%2Dad%2Dcode%2Dletter%2Dformat%2Ejpeg&parent=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments&ga=1',
+		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/2ed56b771e89c3f7f77edc002776bfa8/authorized-dealership-ad-code-letter-format%20%281%29.jpeg',
 	},
 	{
 		doc_name : 'Insurance certificate',
@@ -104,11 +102,11 @@ export const checkFileList = [
 	{
 		doc_name    : 'Acknowledgement copy of LUT of current financial year/GST paid invoice between shipper & consignee',
 		doc_type    : 'company_tax_docs',
-		sample_link : 'https://cogofreight-my.sharepoint.com/personal/aman_kumar_cogoport_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments%2Fletter%2Dof%2Dundertaking%2Dlut%2D%2Ejpeg&parent=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments&ga=1',
+		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/98432eb67e0dd4c09fcaca028fda3658/letter-of-undertaking-lut-%20%281%29.jpeg',
 	},
 	{
 		doc_name    : 'Registration cum Membership certificate of concern authority',
-		doc_type    : 'company_gen_docs',
-		sample_link : 'https://cogofreight-my.sharepoint.com/personal/aman_kumar_cogoport_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments%2FRCMC%20%281%29%2Epdf&parent=%2Fpersonal%2Faman%5Fkumar%5Fcogoport%5Fcom%2FDocuments&ga=1',
+		doc_type    : 'company_general_docs',
+		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/ca59d74941869eb1ebd5ee85fd8d445d/RCMC%20%281%29%20%281%29.pdf',
 	},
 ];

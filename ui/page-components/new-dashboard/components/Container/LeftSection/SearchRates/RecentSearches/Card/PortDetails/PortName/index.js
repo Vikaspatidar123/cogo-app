5372f002061ea4c853f8f07a821b48ff37dc868a
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-07-19 10:27:13 +0530

    chnages

--- ui/page-components/new-dashboard/components/Container/LeftSection/SearchRates/RecentSearches/Card/PortDetails/PortName/index.js
+++ ui/page-components/new-dashboard/components/Container/LeftSection/SearchRates/RecentSearches/Card/PortDetails/PortName/index.js
@@ -6,16 +6,16 @@ function PortName({ location }) {
 	const splitDisplayName = (location.display_name || '').split(',');
 	const displayNameLength = splitDisplayName?.length;
 
-	const info1 = (location.country || {}).name || splitDisplayName[displayNameLength - 1] || '';
+	const info1 = (location?.country || {}).name || splitDisplayName[displayNameLength - 1] || '';
 	const info2 = `${location.port_code}, ${info1}`;
 
 	return (
 		<div className={styles.port_info}>
 			<div className={cl`${styles.text} ${styles.info}`}>{info2}</div>
 			<Tooltip
-				content={<div style={{ color: 'grey' }}>{location.name}</div>}
+				content={<div style={{ color: 'grey' }}>{location?.name}</div>}
 			>
-				<div className={cl`${styles.text} ${styles.name}`}>{location.name.substring(0, 15) || ''}</div>
+				<div className={cl`${styles.text} ${styles.name}`}>{location?.name?.substring(0, 15) || ''}</div>
 			</Tooltip>
 		</div>
 	);

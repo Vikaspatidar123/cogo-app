7d1c858b233a25ca9d5524e44fd99e105a41829c
Kanchan Bhandari <kanchan.bhandari@cogoport.com>
2023-07-20 19:35:57 +0530

    doc name changes

--- ui/page-components/documents/constants/checkFileList.js
+++ ui/page-components/documents/constants/checkFileList.js
@@ -1,34 +1,34 @@
 /* eslint-disable max-len */
 export const checkFileList = [
 	{
-		doc_name : 'Copy of Pan Card of Firm/Company/Propreitor(signed and stamped)',
+		doc_name : 'Pan Card copy of Firm/Company/Propreitor(signed and stamped)',
 		doc_type : 'kyc_docs',
 	},
 	{
-		doc_name : 'Pan of director(signed and stamp)',
+		doc_name : 'Pan of Director(signed and stamped) ',
 		doc_type : 'kyc_docs',
 	},
 	{
-		doc_name : 'Aadhaar of director(signed and stamp)',
+		doc_name : 'Aadhaar of Director(signed and stamp)',
 		doc_type : 'kyc_docs',
 	},
 	{
-		doc_name    : 'IEC copy (signed and stamp)',
+		doc_name    : 'IEC Copy(signed and stamp)',
 		doc_type    : 'kyc_docs',
 		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/c8cfaa65c6d3e59b38d13e0900985e18/IEC_format%20%281%29.png',
 	},
 	{
-		doc_name    : 'Gst registration (signed and stamp)',
+		doc_name    : 'GST Registration(signed and stamp)',
 		doc_type    : 'company_tax_docs',
 		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/95c72c88842372e66c57bbdf221ebeb5/GST-Registration-Certificate-Sample-Annexure-A%20%281%29.png',
 	},
 	{
-		doc_name    : 'Certificate of incorporation',
+		doc_name    : 'Certificate of Incorporation',
 		doc_type    : 'company_registration_docs',
 		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/b0e35c0aa25953e5e08e5644cd396420/Certificate-Of-Incorporation.jpeg',
 	},
 	{
-		doc_name    : 'Memorandum & Article of association',
+		doc_name    : 'Memorandum & Article of Association',
 		doc_type    : 'company_registration_docs',
 		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/02bdaad19978dbb68f367485bfe19bf2/MOA-AOA_sample%20template%20%281%29.png',
 	},
@@ -37,70 +37,70 @@ export const checkFileList = [
 		doc_type : 'company_registration_docs',
 	},
 	{
-		doc_name    : 'Bank Authorization Letter on Bank\'s letterhead duly signed & stamp',
+		doc_name    : 'Bank Authorization Letter on Bank\'s letterhead(signed & stamp)',
 		doc_type    : 'company_financial_docs',
 		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/c49dce5b0e71e0c3d8845b007e46eb9b/Bank%20Authorisation%20Letter%20-%20New%20%281%29.pdf',
 	},
 	{
-		doc_name    : 'GST Return form-3B of Last 3 months(signed and stamped)',
+		doc_name    : 'Last 3 months GST Return Form-3B( signed and stamped)',
 		doc_type    : 'company_tax_docs',
 		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/dc278220e954a2c54cf65be2684c1e23/GSTR-3B-Return-Format%20%281%29.png',
 	},
 	{
-		doc_name    : 'Original Authorization Letter to CHA(original) duly signed and stamped',
+		doc_name    : 'Original Authorization Letter to CHA( signed and stamped)',
 		doc_type    : 'company_general_docs',
 		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/a554c3cdb8e16a1c9de7fa18e95959ff/CHA%20authority%20letter%20format%20copy%20%281%29.tiff',
 	},
 	{
-		doc_name    : 'KYC letter on customer\'s letterhead duly signed &stamped',
+		doc_name    : 'KYC letter on customer\'s letterhead(signed &stamped)',
 		doc_type    : 'kyc_docs',
 		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/739799d6ba4ee67a4098765d32a67058/KYC%20%281%29%20%281%29.pdf',
 	},
 	{
-		doc_name : 'ITR of last 2 years of firm/Propreitor/Directors',
+		doc_name : 'ITR of last 2 years(firm/Propreitor/Directors)',
 		doc_type : 'company_tax_docs',
 	},
 	{
-		doc_name : 'Balance Sheet of last 2 years',
+		doc_name : 'Balance Sheet of last 2 financial years',
 		doc_type : 'company_financial_docs',
 	},
 	{
-		doc_name    : 'Board Resolution if company',
+		doc_name    : 'Board Resolution(only for company)',
 		doc_type    : 'company_registration_docs',
 		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/b65deadb0adb1a7216ff06ab13ab2161/Template-NonGovt-BoardResolution-2021_page-0001-724x1024%20%281%29.jpeg',
 	},
 	{
-		doc_name : 'Partnership deed or certificate of incorporation if LLP orIndividual Owner',
+		doc_name : 'Partnership deed or Certificate of Incorporation(for LLP or Individual Owner)',
 		doc_type : 'company_registration_docs',
 	},
 	{
-		doc_name : 'Address Proof i.e. Electricity Bill, Landline Telephone Bill, Rent Agreement etc',
+		doc_name : 'Address Proof (Electricity Bill/Landline Telephone Bill/Rent Agreement)',
 		doc_type : 'kyc_docs',
 	},
 	{
-		doc_name    : 'Request Letter Addressed to Assistant/DeputyCommissioner of Custom at Desired Port',
+		doc_name    : 'Request Letter Addressed to Assistant/Deputy Commissioner of Custom at Desired Port',
 		doc_type    : 'company_general_docs',
 		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/ab637cead9e949aada5115faf89871b4/Covering%20Letter-Request%20letter%20to%20Assis%3Adeputy%20commissionar.doc',
 	},
 	{
-		doc_name : 'Bank Statement of last 6 Months',
+		doc_name : 'Last 6 Months Bank Statement',
 		doc_type : 'company_financial_docs',
 	},
 	{
-		doc_name : 'Canceled Cheque',
+		doc_name : 'Cancelled Cheque',
 		doc_type : 'company_financial_docs',
 	},
 	{
-		doc_name    : 'Original AD code letter From Bank',
+		doc_name    : 'Original AD code letter from Bank',
 		doc_type    : 'company_financial_docs',
 		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/2ed56b771e89c3f7f77edc002776bfa8/authorized-dealership-ad-code-letter-format%20%281%29.jpeg',
 	},
 	{
-		doc_name : 'Insurance certificate',
+		doc_name : 'Insurance Certificate',
 		doc_type : 'company_financial_docs',
 	},
 	{
-		doc_name    : 'Acknowledgement copy of LUT of current financial year/GST paid invoice between shipper & consignee',
+		doc_name    : 'Acknowledgement copy of LUT of current financial year',
 		doc_type    : 'company_tax_docs',
 		sample_link : 'https://cogoport-production.sgp1.digitaloceanspaces.com/98432eb67e0dd4c09fcaca028fda3658/letter-of-undertaking-lut-%20%281%29.jpeg',
 	},

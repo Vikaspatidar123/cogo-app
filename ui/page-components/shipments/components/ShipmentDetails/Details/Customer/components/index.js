01360944834c4e2e249592ef32d982c8f4cd69b5
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-07-20 19:55:56 +0530

    revert

--- ui/page-components/shipments/components/ShipmentDetails/Details/Customer/components/index.js
+++ ui/page-components/shipments/components/ShipmentDetails/Details/Customer/components/index.js
@@ -1,5 +1,5 @@
 import { isEmpty } from '@cogoport/utils';
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import styles from './styles.module.css';
 import TabSections from './Tabs';

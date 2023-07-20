721dcef468354d9e3fee8371fbb7c37b680a9f20
shivanibairoju <shivani.bairoju@cogoport.com>
2023-07-20 18:06:45 +0530

    pr changes

--- ui/page-components/profile/components/ProfileDetails/AccountDetails/DetailsTabs/Address/BillingAddresses/BillingAddressCard/PocCard/index.js
+++ ui/page-components/profile/components/ProfileDetails/AccountDetails/DetailsTabs/Address/BillingAddresses/BillingAddressCard/PocCard/index.js
@@ -4,6 +4,19 @@ import { useTranslation } from 'next-i18next';
 
 import styles from './styles.module.css';
 
+function PocSubContainer({ value, label }) {
+	return (
+		<div className={styles.poc_sub_container}>
+			<div className={cl`${styles.value_text} ${styles.poc_details}`}>
+				{label}
+			</div>
+			<div className={cl`${styles.label_text} ${styles.poc_details}`}>
+				{value}
+			</div>
+		</div>
+	);
+}
+
 function PocCard({ firstPoc = {}, setPocToUpdate = () => {}, setShowPocModal = () => {} }) {
 	const { t } = useTranslation(['settings']);
 
@@ -22,50 +35,27 @@ function PocCard({ firstPoc = {}, setPocToUpdate = () => {}, setShowPocModal = (
 				<IcMEdit style={{ width: 12, height: 12 }} />
 			</div>
 
-			<div className={styles.poc_sub_container}>
-				<div className={cl`${styles.value_text} ${styles.poc_details}`}>
-					{t('settings:poc_name_label')}
-				</div>
-				<div className={cl`${styles.label_text} ${styles.poc_details}`}>
-					{firstPoc?.name || '-'}
-				</div>
-			</div>
+			<PocSubContainer label={t('settings:poc_name_label')} value={firstPoc?.name || '-'} />
 
-			<div className={styles.poc_sub_container}>
-				<div className={cl`${styles.value_text} ${styles.poc_details}`}>
-					{t('settings:poc_mobile_label')}
-				</div>
-				<div className={`${styles.label_text} ${styles.poc_details}`}>
-					{firstPoc?.mobile_number
-						? `${firstPoc?.mobile_country_code || ''} ${
-							firstPoc?.mobile_number
-						}`
-						: '-'}
-				</div>
-			</div>
+			<PocSubContainer
+				label={t('settings:poc_mobile_label')}
+				value={firstPoc?.mobile_number
+					? `${firstPoc?.mobile_country_code || ''} ${
+						firstPoc?.mobile_number
+					}`
+					: '-'}
+			/>
 
-			<div className={styles.poc_sub_container}>
-				<div className={cl`${styles.value_text} ${styles.poc_details}`}>
-					{t('settings:poc_email_label')}
-					{' '}
-				</div>
-				<div className={cl`${styles.label_text} ${styles.poc_details}`}>
-					{firstPoc?.email || '-'}
-				</div>
-			</div>
+			<PocSubContainer label={t('settings:poc_email_label')} value={firstPoc?.email || '-'} />
 
-			<div className={styles.poc_sub_container}>
-				<div className={cl`${styles.value_text} ${styles.poc_details}`}>
-					{t('settings:poc_alternate_mobile_label')}
-				</div>
-				<div className={cl`${styles.label_text} ${styles.poc_details}`}>
-					{firstPoc?.alternate_mobile_number
-						? `${firstPoc?.alternate_mobile_country_code || ''} ${
-							firstPoc?.alternate_mobile_number
-						}`
-						: '-'}
-				</div>
-			</div>
+			<PocSubContainer
+				label={t('settings:poc_alternate_mobile_label')}
+				value={firstPoc?.alternate_mobile_number
+					? `${firstPoc?.alternate_mobile_country_code || ''} ${
+						firstPoc?.alternate_mobile_number
+					}`
+					: '-'}
+			/>
 		</div>
 	);
 }

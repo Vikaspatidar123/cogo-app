093bf3022b7cfa79f7e553c71cc0504539719aa4
Kanchan Bhandari <kanchan.bhandari@cogoport.com>
2023-07-25 18:29:10 +0530

    fixes

--- ui/page-components/documents/components/ServiceUploadDocument/InnerForm/index.js
+++ ui/page-components/documents/components/ServiceUploadDocument/InnerForm/index.js
@@ -7,6 +7,24 @@ import styles from './styles.module.css';
 import { useForm } from '@/packages/forms';
 import getField from '@/packages/forms/Controlled';
 
+const getMutatedFields = (mandatory_validity_docs, value, t) => {
+	const mutatedControls = [];
+	formControls.forEach((cont) => {
+		const controlItem = cont || {};
+
+		if (controlItem.name === 'doc_validity') {
+			if (mandatory_validity_docs.includes(value?.doc_name)) {
+				controlItem.rules = { required: { value: true, message: t('documents:document_validity_err_msz') } };
+			} else {
+				controlItem.rules = undefined;
+			}
+		}
+		mutatedControls.push(controlItem);
+	});
+
+	return mutatedControls;
+};
+
 function InnerForm({
 	value = {},
 	setActiveCollapse = () => {},
@@ -24,24 +42,6 @@ function InnerForm({
 		t('documents:custom_service_doc_8'),
 		t('documents:custom_service_doc_15'),
 	];
-
-	const getMutatedFields = () => {
-		const mutatedControls = [];
-		formControls.forEach((cont) => {
-			const controlItem = cont || {};
-
-			if (controlItem.name === 'doc_validity') {
-				if (mandatory_validity_docs.includes(value?.doc_name)) {
-					controlItem.rules = { required: { value: true, message: 'Document Validity is required' } };
-				} else {
-					controlItem.rules = undefined;
-				}
-			}
-			mutatedControls.push(controlItem);
-		});
-
-		return mutatedControls;
-	};
 
 	const {
 		handleSubmit,
@@ -63,7 +63,7 @@ function InnerForm({
 	return (
 		<div className={styles.inner_form}>
 			<div className={styles.form}>
-				{getMutatedFields(value).map((item) => {
+				{getMutatedFields(mandatory_validity_docs, value, t).map((item) => {
 					const { label, name, component } = item || {};
 					const Element = getField(component);
 
--- ui/page-components/documents/constants/checkFileList.js
+++ ui/page-components/documents/constants/checkFileList.js
@@ -16,22 +16,22 @@ export const checkFileList = (t) => [
 	{
 		doc_name    : t('documents:custom_service_doc_4'),
 		doc_type    : 'kyc_docs',
-		sample_link : GLOBAL_CONSTANTS.organization_documents_sample_link.iec,
+		sample_link : GLOBAL_CONSTANTS.image_url.iec,
 	},
 	{
 		doc_name    : t('documents:custom_service_doc_5'),
 		doc_type    : 'company_tax_docs',
-		sample_link : GLOBAL_CONSTANTS.organization_documents_sample_link.gst,
+		sample_link : GLOBAL_CONSTANTS.image_url.gst,
 	},
 	{
 		doc_name    : t('documents:custom_service_doc_6'),
 		doc_type    : 'company_registration_docs',
-		sample_link : GLOBAL_CONSTANTS.organization_documents_sample_link.incorporation_certificate,
+		sample_link : GLOBAL_CONSTANTS.image_url.incorporation_certificate,
 	},
 	{
 		doc_name    : t('documents:custom_service_doc_7'),
 		doc_type    : 'company_registration_docs',
-		sample_link : GLOBAL_CONSTANTS.organization_documents_sample_link.memorandum,
+		sample_link : GLOBAL_CONSTANTS.image_url.memorandum,
 	},
 	{
 		doc_name : t('documents:custom_service_doc_8'),
@@ -40,22 +40,22 @@ export const checkFileList = (t) => [
 	{
 		doc_name    : t('documents:custom_service_doc_9'),
 		doc_type    : 'company_financial_docs',
-		sample_link : GLOBAL_CONSTANTS.organization_documents_sample_link.bank_letterhead,
+		sample_link : GLOBAL_CONSTANTS.image_url.bank_letterhead,
 	},
 	{
 		doc_name    : t('documents:custom_service_doc_10'),
 		doc_type    : 'company_tax_docs',
-		sample_link : GLOBAL_CONSTANTS.organization_documents_sample_link.gst_return_form_3b,
+		sample_link : GLOBAL_CONSTANTS.image_url.gst_return_form_3b,
 	},
 	{
 		doc_name    : t('documents:custom_service_doc_11'),
 		doc_type    : 'company_general_docs',
-		sample_link : GLOBAL_CONSTANTS.organization_documents_sample_link.authorization_letter_cha,
+		sample_link : GLOBAL_CONSTANTS.image_url.authorization_letter_cha,
 	},
 	{
 		doc_name    : t('documents:custom_service_doc_12'),
 		doc_type    : 'kyc_docs',
-		sample_link : GLOBAL_CONSTANTS.organization_documents_sample_link.kyc,
+		sample_link : GLOBAL_CONSTANTS.image_url.kyc,
 	},
 	{
 		doc_name : t('documents:custom_service_doc_13'),
@@ -68,7 +68,7 @@ export const checkFileList = (t) => [
 	{
 		doc_name    : t('documents:custom_service_doc_15'),
 		doc_type    : 'company_registration_docs',
-		sample_link : GLOBAL_CONSTANTS.organization_documents_sample_link.board_resolution,
+		sample_link : GLOBAL_CONSTANTS.image_url.board_resolution,
 	},
 	{
 		doc_name : t('documents:custom_service_doc_16'),
@@ -81,7 +81,7 @@ export const checkFileList = (t) => [
 	{
 		doc_name    : t('documents:custom_service_doc_18'),
 		doc_type    : 'company_general_docs',
-		sample_link : GLOBAL_CONSTANTS.organization_documents_sample_link.letter_to_custom_commissioner,
+		sample_link : GLOBAL_CONSTANTS.image_url.letter_to_custom_commissioner,
 	},
 	{
 		doc_name : t('documents:custom_service_doc_19'),
@@ -94,7 +94,7 @@ export const checkFileList = (t) => [
 	{
 		doc_name    : t('documents:custom_service_doc_21'),
 		doc_type    : 'company_financial_docs',
-		sample_link : GLOBAL_CONSTANTS.organization_documents_sample_link.ad_code_letter,
+		sample_link : GLOBAL_CONSTANTS.image_url.ad_code_letter,
 	},
 	{
 		doc_name : t('documents:custom_service_doc_22'),
@@ -103,11 +103,11 @@ export const checkFileList = (t) => [
 	{
 		doc_name    : t('documents:custom_service_doc_23'),
 		doc_type    : 'company_tax_docs',
-		sample_link : GLOBAL_CONSTANTS.organization_documents_sample_link.lut_copy,
+		sample_link : GLOBAL_CONSTANTS.image_url.lut_copy,
 	},
 	{
 		doc_name    : t('documents:custom_service_doc_24'),
 		doc_type    : 'company_general_docs',
-		sample_link : GLOBAL_CONSTANTS.organization_documents_sample_link.membership_certificate,
+		sample_link : GLOBAL_CONSTANTS.image_url.membership_certificate,
 	},
 ];

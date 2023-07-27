3e053b0046119f460bb01883c38b6dc3b62acaf7
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-07-27 15:46:05 +0530

    changes

--- ui/page-components/contract-management/utils/getFormattedBookingPayload.js
+++ ui/page-components/contract-management/utils/getFormattedBookingPayload.js
@@ -1,7 +1,10 @@
 import { differenceInDays } from '@cogoport/utils';
 
+const MAIN_SERVICES = ['lcl_freight', 'air_freight'];
+
 export const formattedBookingPayload = ({ item = {}, data = {}, contractId = '' }) => {
 	const { service_id, service_type, contract_services_utilisation_id } =		data || {};
+	const { trucks_count = 0, weight, volume, packages_count, primary_service_id } = item || {};
 
 	return {
 		service_id,
@@ -12,6 +15,17 @@ export const formattedBookingPayload = ({ item = {}, data = {}, contractId = '' 
 		source : 'contract',
 		data   : {
 			...item,
+			...(MAIN_SERVICES.includes(service_type) && {
+				attributes: [
+					{
+						weight,
+						volume,
+						packages_count,
+						primary_service_id,
+					},
+				],
+			}),
+			trucks_count : trucks_count || undefined,
 			bls_count    : 1,
 			bl_type      : 'rfs',
 			transit_time : Math.abs(

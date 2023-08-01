add3e20b0e065e113b617d635783f67ba64be904
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-08-01 15:26:42 +0530

    fix create spot search api payload

--- ui/page-components/quotation/CreateQuotation/utils/getRatesModal.js
+++ ui/page-components/quotation/CreateQuotation/utils/getRatesModal.js
@@ -1,4 +1,5 @@
 import { Toast } from '@cogoport/components';
+import { isEmpty } from '@cogoport/utils';
 import { useState } from 'react';
 
 const MAPPING = {
@@ -9,25 +10,36 @@ const MAPPING = {
 	volume         : 'Container volume',
 	quantity       : 'Package Count',
 };
+
 const useGetRatesModal = ({ current, setCalculateCharge }) => {
 	const [data, setData] = useState([]);
+
 	const getRatesModalHandler = async () => {
+		const errors = [];
+
 		const promises = current.transport.handleSubmit();
 		const details = current.details.handleSubmit();
 		const product = current.product.handleSubmit();
 		const promiseValues = await Promise.all([promises, details, product]);
 
-		const check = promiseValues.map((item) => item.check);
-		const error = promiseValues.map(
-			(e) => e?.err !== undefined && Object.keys(e?.err),
-		);
-		const errKey = error.flat().filter((item) => item !== false);
-		const ne = errKey.map((x) => MAPPING[x]);
-		if (check.includes(true)) {
-			Toast.error(`Required ${ne.join()}`);
+		promiseValues.forEach((value) => {
+			if (value?.check && value?.err) {
+				errors.push(Object.keys(value?.err));
+			}
+		});
+
+		const err = errors.flat().map((name) => MAPPING[name]);
+
+		if (!current.date) {
+			Toast.error('Please enter Expiry Date');
 			return false;
 		}
-		setData(promiseValues);
+
+		if (!isEmpty(errors)) {
+			Toast.error(`Please enter  ${err.join(', ')}`);
+			return false;
+		}
+		setData([...promiseValues, { expiryDate: current.date }]);
 		setCalculateCharge(true);
 
 		return true;

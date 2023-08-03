b4f911f3c2260a818e55718c2e098d04f9528c68
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-08-03 16:26:01 +0530

    pr changes

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,6 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
+import GLOBAL_CONSTANTS from '@/ui/commons/constants/globals';
 import { Signup } from '@/ui/page-components/authentication';
 
 export async function getServerSideProps(ctx) {
@@ -7,7 +8,7 @@ export async function getServerSideProps(ctx) {
 	let IP_CLIENT = ctx?.req?.headers['x-forwarded-for'] || null;
 
 	if (IP_CLIENT?.includes(',')) {
-		IP_CLIENT = IP_CLIENT?.split(',')?.[0] || null;
+		IP_CLIENT = IP_CLIENT?.split(',')?.[GLOBAL_CONSTANTS.zeroth_index] || null;
 	}
 	return {
 		props: {
--- ui/page-components/authentication/utils/getFormattedPayload.js
+++ ui/page-components/authentication/utils/getFormattedPayload.js
@@ -1,6 +1,7 @@
-const getFormattedPayload = (props) => {
-	const { formValues = {}, leadUserId = '', is_whatsapp_number = false } = props;
+const getFormattedPayload = async (props) => {
+	const { formValues = {}, leadUserId = '', is_whatsapp_number = false, recaptchaRef } = props;
 	const { name, email, mobile_number } = formValues;
+	const google_recaptcha_response = await recaptchaRef?.current?.executeAsync();
 	return {
 		lead_user_id        : leadUserId || undefined,
 		name                : name || undefined,
@@ -8,6 +9,7 @@ const getFormattedPayload = (props) => {
 		mobile_country_code : mobile_number?.country_code || undefined,
 		mobile_number       : mobile_number?.number || undefined,
 		is_whatsapp_number,
+		google_recaptcha_response,
 	};
 };
 

bd10d5135cbc102d45b6678523aeb3e06bddc7f5
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-08-11 14:54:50 +0530

    translation

--- ui/page-components/profile/components/ProfileDetails/AccountDetails/DetailsTabs/AlertsPreferences/ShipmentAlerts/components/List/Schedule/Details/EditDetails/SelectUserlist/UserTable/Header/index.js
+++ ui/page-components/profile/components/ProfileDetails/AccountDetails/DetailsTabs/AlertsPreferences/ShipmentAlerts/components/List/Schedule/Details/EditDetails/SelectUserlist/UserTable/Header/index.js
@@ -1,10 +1,18 @@
 import { Checkbox } from '@cogoport/components';
+import { useTranslation } from 'next-i18next';
 
 import styles from './styles.module.css';
 
-const HEADER = ['name', 'designation', 'Email Id', 'mobile no.'];
+const HEADER = ({ t = () => {} }) => [
+	t('settings:schedule_table_option_1'),
+	t('settings:schedule_table_option_2'),
+	t('settings:schedule_table_option_3'),
+	t('settings:schedule_table_option_4'),
+];
 
 function Header({ setUserIds, list }) {
+	const { t } = useTranslation(['settings']);
+
 	const onCheck = (value) => {
 		if (value?.target.checked) {
 			setUserIds(list.map((item) => item.user_id));
@@ -12,13 +20,14 @@ function Header({ setUserIds, list }) {
 			setUserIds([]);
 		}
 	};
+	const coloums = HEADER({ t });
 	return (
 		<div className={styles.container}>
 			<div className={styles.header}>
 				<div className={styles.checkbox}>
 					<Checkbox onChange={onCheck} />
 				</div>
-				{HEADER.map((item) => <div key={item} className={styles.box}>{item}</div>)}
+				{coloums.map((item) => <div key={item} className={styles.box}>{item}</div>)}
 			</div>
 
 		</div>

d6ce52e53851972c4869cd795a05096f8bd9026f
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-08-17 13:54:43 +0530

    changes in get countries

--- scripts/build-scripts/countries/index.js
+++ scripts/build-scripts/countries/index.js
@@ -16,7 +16,7 @@ const setCountries = async () => {
 	await getCountries(
 		fs.writeFileSync,
 		countriesPath,
-		'https://api.cogoport.com/',
+		'https://api-meteora1.dev.cogoport.io/',
 	);
 	console.log('Successfully Built Countries ...');
 };

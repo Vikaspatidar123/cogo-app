c3ff2ca33b28ee2198cf2f10a36fa2b0ca69472f
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-08-18 10:27:31 +0530

    comment alerts

--- ui/page-components/profile/components/ProfileDetails/AccountDetails/utils/alerts-mapping.js
+++ ui/page-components/profile/components/ProfileDetails/AccountDetails/utils/alerts-mapping.js
@@ -3,12 +3,12 @@ import ShipmentAlerts from '../DetailsTabs/AlertsPreferences/ShipmentAlerts';
 
 const getOptions = ({ t = () => { } }) => (
 	{
-		shipment_alerts: {
-			key                : 'shipment_alerts',
-			title              : t('settings:shipment_alerts_text'),
-			containerComponent : ShipmentAlerts,
+		// shipment_alerts: {
+		// 	key                : 'shipment_alerts',
+		// 	title              : t('settings:shipment_alerts_text'),
+		// 	containerComponent : ShipmentAlerts,
 
-		},
+		// },
 		promotional_alerts: {
 			key                : 'promotional_alerts',
 			title              : t('settings:promotional_alerts_text'),
